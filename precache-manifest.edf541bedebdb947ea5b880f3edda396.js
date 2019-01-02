4a6e5aa5ec3ed7063f391a03cce53bfe96905220
jomorro <morrowjoe0@gmail.com>
2019-01-01 20:38:53 -0500

    Updates

---
+++ precache-manifest.edf541bedebdb947ea5b880f3edda396.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1602c1e071ba32c1ffee",
+    "url": "/tic_tac_toe_react/static/js/runtime~main.1602c1e0.js"
+  },
+  {
+    "revision": "c607a3e2f2d82919b001",
+    "url": "/tic_tac_toe_react/static/js/main.c607a3e2.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/tic_tac_toe_react/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "c607a3e2f2d82919b001",
+    "url": "/tic_tac_toe_react/static/css/main.3c7d473a.chunk.css"
+  },
+  {
+    "revision": "15232b32adaf168021afdd88077ffb54",
+    "url": "/tic_tac_toe_react/index.html"
+  }
+];
